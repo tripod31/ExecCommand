c871ac0f26d5e7c742b6cf900b58d1f8ce8a1140
tripod31 <tripod31@hotmail.com>
2016-01-22 13:34:18 +0900

    upd

--- sample/remote/test_client.pl
+++ sample/remote/test_client.pl
@@ -1,7 +1,7 @@
 use strict;
 
 use Win32::OLE qw(in with);
-my $obj = Win32::OLE->new('ClassLibraryForVBA.ExecCommand') ;
+my $obj = Win32::OLE->new('ExecCommand.ExecCommand') ;
 $obj->InitRemoteClient();
 
 my $msg=$obj->{"RemoteData"};
