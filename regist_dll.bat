23b2908132189060eecfff66ac981f4012d5e7ef
tripod31 <tripod31@hotmail.com>
2018-03-11 21:32:31 +0900

    installerを追加

--- regist_dll.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set REGASM=C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe
-
-REM カレントディレクトリをバッチファイルのディレクトリにする。
-cd /d %~dp0
-%REGASM% /codebase bin\release\ExecCommand.dll
-pause
--- unregist_dll.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set REGASM=C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\regasm.exe
-REM カレントディレクトリをバッチファイルのディレクトリにする。
-cd /d %~dp0
-%REGASM% bin\Release\ExecCommand.dll /unregister /verbose
-pause
