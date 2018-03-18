9033d9348028846cbc65c78895edda39160c531c
tripod31 <tripod31@hotmail.com>
2018-03-18 18:11:20 +0900

    add exe.config to zip

--- register/Properties/AssemblyInfo.cs
+++ register/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("register")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("yoshi")]
 [assembly: AssemblyProduct("register")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
